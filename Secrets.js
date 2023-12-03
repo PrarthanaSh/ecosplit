6d02e77a722552a4230fdc93fa50130845cdd0f7
Prarthana Shevatekar <107344416+PrarthanaSh@users.noreply.github.com>
2023-12-03 18:12:36 -0500

    Delete Secrets.js
    Secrets deleted

--- Secrets.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB0Gff4Z4ZkuM2oncKspaexZlFiaL-MCCI",
-  authDomain: "ecosplit-bf28d.firebaseapp.com",
-  projectId: "ecosplit-bf28d",
-  storageBucket: "ecosplit-bf28d.appspot.com",
-  messagingSenderId: "92202690168",
-  appId: "1:92202690168:web:137809448af33da33d6f8d"
-};
